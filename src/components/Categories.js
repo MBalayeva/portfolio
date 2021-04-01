7120118fc6bf3e61cf82f9c49104cc42a2b36330
MBalayeva <mikabalayeva1984@gmail.com>
2021-04-01 13:57:31 +0400

    final touches to portfolio page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function Categories() {
+function Categories({filter, categories}) {
     return (
-        <div>
-            
+        <div className='btns'>
+            {categories.map((cat, id) => {
+                return (
+                  <button className="portfolio-btn" onClick={()=>filter(cat)} key={id}>
+                    {cat}
+                  </button>
+                );
+            })}
         </div>
     )
 }
--- src/pages/PortfoliosPage.js
+++ src/pages/PortfoliosPage.js
@@ -5,40 +5,53 @@ import Categories from '../components/Categories';
 import portfolios from '../components/allportfolios';
 
 function PortfoliosPage() {
-    const [menuItems, setMenuItems] = useState(portfolios);
-    const [categories, setCategories] = useState(null);
+  const allCategories = ['All', ...new Set(portfolios.map((item) => item.category))];
 
-    return (
-      <div className="PortfoliosPage">
-        <div className="title">
-          <Title span="portfolios" title="portfolios" />
-        </div>
-        <div className="portfolios-data">
-          <Categories />
-          <div className="portfolios">
-            <div className="container">
-              <div className="row">
-                {menuItems.map((item) => {
-                  return (
-                    <div className="col-md-5" key={item.id}>
-                      <MenuItems
-                        title={item.title}
-                        image={item.image}
-                        link1={item.link1}
-                        link2={item.link2}
-                        icon1={item.icon1}
-                        icon2={item.icon2}
-                        key={item.id}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
+  const [menuItems, setMenuItems] = useState(portfolios);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filter = (category) => {
+    if(category === 'All') {
+      setMenuItems(portfolios);
+      return;
+    }
+    const filteredCategories = portfolios.filter(portfolio => {
+      return portfolio.category === category;
+    })
+    setMenuItems(filteredCategories);
+  }
+
+  return (
+    <div className="PortfoliosPage">
+      <div className="title">
+        <Title span="portfolios" title="portfolios" />
+      </div>
+      <div className="portfolios-data">
+        <Categories filter={filter} categories={categories }/>
+        <div className="portfolios">
+          <div className="container">
+            <div className="row">
+              {menuItems.map((item) => {
+                return (
+                  <div className="col-md-5" key={item.id}>
+                    <MenuItems
+                      title={item.title}
+                      image={item.image}
+                      link1={item.link1}
+                      link2={item.link2}
+                      icon1={item.icon1}
+                      icon2={item.icon2}
+                      key={item.id}
+                    />
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default PortfoliosPage
